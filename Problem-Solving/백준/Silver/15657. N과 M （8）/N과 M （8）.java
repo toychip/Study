31cead2d9329a4cae89647ae85ff367c14ddc0b6
Junhyoung Lim <109949924+toychip@users.noreply.github.com>
2025-01-05 17:44:22 +0900

    [Silver III] Title: N과 M (8), Time: 156 ms, Memory: 18000 KB -BaekjoonHub

---
+++ Problem-Solving/백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    static int[] number;
+    static int[] array;
+    static int N;
+    static int M;
+    static BufferedWriter writer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input1 = reader.readLine().split(" ");
+        String[] input2 = reader.readLine().split(" ");
+
+        N = Integer.parseInt(input1[0]);
+        M = Integer.parseInt(input1[1]);
+
+        number = new int[N];
+        array = new int[M];
+
+        for (int i = 0; i < N; i++) {
+            number[i] = Integer.parseInt(input2[i]);
+        }
+
+        Arrays.sort(number);
+
+        backtrack(0, 0);
+        writer.flush();
+        writer.close();
+    }
+
+    private static void backtrack(final int depth, final int start) throws IOException {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                writer.write(String.valueOf(array[i]));
+                writer.write(" ");
+            }
+            writer.write("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            array[depth] = number[i];
+            backtrack(depth + 1, i);
+        }
+    }
+}
