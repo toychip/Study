f1bf018816119ba47f67bac931dfd71d26894e9d
toychip <dlawnsgud427@naver.com>
2025-01-15 16:40:15 +0900

    MSA/섹션11: FeignClient 사용

--- 스프링 Cloud로 개발하는 MSA/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ 스프링 Cloud로 개발하는 MSA/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,14 +1,17 @@
 package com.example.userservice.service;
 
+import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,6 +24,7 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
+    private final OrderServiceClient orderServiceClient;
 
     @Override
     public UserDto createUser(final UserDto userDto) {
@@ -51,7 +55,8 @@ public class UserServiceImpl implements UserService {
 
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
 
-        ArrayList<ResponseOrder> orders = new ArrayList<>();
+//        ArrayList<ResponseOrder> orders = new ArrayList<>();
+        List<ResponseOrder> orders = orderServiceClient.getOrders(userId);
 
         userDto.setOrders(orders);
 
