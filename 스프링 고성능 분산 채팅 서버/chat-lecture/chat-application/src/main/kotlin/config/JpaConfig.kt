0567273abb87ce0c7ead1a6f30c7f2a1242d9316
toychip <dlawnsgud427@naver.com>
2025-08-09 20:07:44 +0900

    분산채팅/섹션3: chat-application 모듈 구성

---
+++ 스프링 고성능 분산 채팅 서버/chat-lecture/chat-application/src/main/kotlin/config/JpaConfig.kt
@@ -0,0 +1,12 @@
+package com.chat.application.config
+
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = ["com.chat.persistence.repository"])
+@EntityScan(basePackages = ["com.chat.domain.model"])
+class JpaConfig
---
+++ 스프링 고성능 분산 채팅 서버/chat-lecture/settings.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+rootProject.name = "chat-lecture"
+
+include(
+    "chat-application"
+)
