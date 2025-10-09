5ba17f05a251cc7ea9a6e5b5e7b63e61064895e3
toychip <dlawnsgud427@naver.com>
2025-10-09 19:13:02 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- Problem-Solving/백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ Problem-Solving/백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,29 +1,29 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.Arrays;
 
 public class Main {
 
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(reader.readLine());
-        for (int i = 0; i < N; i++) {
-            int n = Integer.parseInt(reader.readLine());
-            int[] dp = new int[n + 1];
+        int n = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < n; i++) {
+            dp = new int[20];
             Arrays.fill(dp, -1);
-            int answer = recur(0, n, dp);
+            int newOne = Integer.parseInt(reader.readLine());
+            int answer = solve(0, newOne);
             writer.write(String.valueOf(answer));
             writer.write("\n");
         }
+
         writer.flush();
         writer.close();
     }
 
-    private static int recur(final int level, final int goal, final int[] dp) {
+    private static int solve(int level, int goal) {
         if (level == goal) {
             return 1;
         }
@@ -36,10 +36,7 @@ public class Main {
             return dp[level];
         }
 
-        dp[level] = recur(level + 1, goal, dp) +
-                recur(level + 2, goal, dp) +
-                recur(level + 3, goal, dp);
-
+        dp[level] = solve(level + 3, goal) + solve(level + 2, goal) + solve(level + 1, goal);
         return dp[level];
     }
 }
